668e5e06ac8356bee4e72790fb17c953fd217c41
Rafi-PC\Rafi <rafo_bg9800@abv.bg>
2020-06-04 19:59:49 +0300

    first point

---
+++ demoRafi/src/test/java/master/soft/tools/DemoRafiApplicationTests.java
@@ -0,0 +1,13 @@
+package master.soft.tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRafiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/master/soft/tools/DemoRafiApplication.java
@@ -0,0 +1,13 @@
+package master.soft.tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRafiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRafiApplication.class, args);
+	}
+
+}
